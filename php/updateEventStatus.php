306689d5c5ec98ca608f788cbf22cfa0e20f8d8e
XiaoWei <xiaoweii7766@gmail.com>
2025-10-21 21:51:42 +0800

    removed duplicated code

--- php/updateEventStatus.php
+++
@@ -1,7 +0,0 @@
-<!-- STILL TESTING -->
-<?php
-
-$autoCloseQuery = "UPDATE tblevents SET status = 'closed' WHERE endDate < CURDATE() AND status NOT IN ('cancelled', 'closed')";
-$connection->query($autoCloseQuery);
-
-?>
