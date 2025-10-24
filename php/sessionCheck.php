1dde378399807b2f0649b4777b7461f75a7fe555
XiaoWei <xiaoweii7766@gmail.com>
2025-10-24 17:57:30 +0800

    sessionCheck page icon

--- php/sessionCheck.php
+++ php/sessionCheck.php
@@ -16,6 +16,7 @@ if (!isset($_SESSION['userID'])) {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>ReLeaf - Access Denied</title>
+        <link rel="icon" type="image/png" href="../../assets/images/Logo.png">
         <link rel="stylesheet" href="../../style/style.css">
         <style>
             .notification {
